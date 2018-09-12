32d35dec5c32e59e4a7059b4fc290737b2e7a641
Alex <alexandervandoren@yahoo.com>
2018-09-12 10:55:53 +0200

    Added fonts

--- src/components/MainLayout/index.jsx
+++ src/components/MainLayout/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Overview from '../Overview'
 import VlaioHeader from '../VlaioHeader'
+import VlaioFooter from '../VlaioFooter'
 
 const MainLayout = () => (
   <div className="mainLayout">
       <VlaioHeader />
       <Overview />  
+      <VlaioFooter />
       
   </div>
 );
