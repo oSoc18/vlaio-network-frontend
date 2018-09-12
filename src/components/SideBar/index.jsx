c98b0797f5c2efbfffd70afbe9d261b82cd08ca0
Alex <alexandervandoren@yahoo.com>
2018-09-12 11:09:35 +0200

    Override default React css

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './style.css';
 
+/**
+ *    TODO: get sidenav based on the type of shown visualisation 
+ *    (overview, network of interactions,recommendations)
+ */
 const Sidebar = () => (
-  <div className="sidenav"></div>
+  <div className="sidenav">
+    <h1>Filter</h1>
+  </div>
 );
 
 export default Sidebar;
