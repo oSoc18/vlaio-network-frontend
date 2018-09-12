c2efef65140b1403d5939eaf67dfc7523091874c
Michiel Leyman <michiel.leyman@gmail.com>
2018-09-12 16:15:50 +0200

    Add UpSet scaffolding

--- src/components/Overview/index.jsx
+++ src/components/Overview/index.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { api } from '../../constants';
+import dummydata from './dummy';
+import UpSetPlot from './UpSetPlot';
 
 import '../../assets/styles/overview.css';
 
-const Overview = () => (
-  <div className="main-content overview">
-    Overview
-  </div>
-);
+class Overview extends Component {
+  state = {
+    overlaps: dummydata
+  };
+
+  componentDidMount() {
+    // api.overlap.get().then(overlap => console.log(overlap));
+  }
+
+  render() {
+    const { overlaps } = this.state;
+    return (
+      <div className="main-content overview">
+        <svg className="overview__plot" width="100%" height="">
+          <UpSetPlot overlaps={overlaps} />
+        </svg>
+      </div>
+    );
+  }
+}
 
 export default Overview;
