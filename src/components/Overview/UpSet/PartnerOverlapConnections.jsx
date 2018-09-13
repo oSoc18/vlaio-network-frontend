57b1bc9782bea1707e42cc4b5fed14738bf5ac6d
Michiel Leyman <michiel.leyman@gmail.com>
2018-09-13 22:31:01 +0200

    Render overlap connections

--- src/components/Overview/UpSet/PartnerOverlapConnections.jsx
+++ src/components/Overview/UpSet/PartnerOverlapConnections.jsx
@@ -13,10 +13,10 @@ const PartnerOverlapConnections = ({
   >
     <g>
       {
-        allPartners.map((partner, i) => (
-          overlaps.map((overlap, j) => (
+        allPartners.map(partner => (
+          overlaps.map(overlap => (
             <circle
-              key={overlap.id}
+              key={`circle-${overlap.id}`}
               cx={xScale(overlap.partners) + 8}
               cy={yScale(partner) + 12}
               r={10}
@@ -24,6 +24,30 @@ const PartnerOverlapConnections = ({
             />
           ))
         ))
+      }
+    </g>
+    <g>
+      {
+        overlaps.map((overlap) => {
+          const { partners } = overlap;
+          return partners.map((partner, i) => (
+            <g key={`${partner}-${overlap.id}`}>
+              <circle
+                cx={xScale(partners) + 8}
+                cy={yScale(partner) + 12}
+                r={10}
+                fill="#000"
+              />
+              <line
+                x1={xScale(partners) + 8}
+                x2={xScale(partners) + 8}
+                y1={yScale(partner) + 12}
+                y2={yScale(partners[i + 1] || partner) + 12}
+                className="overlap-connector"
+              />
+            </g>
+          ));
+        })
       }
     </g>
   </g>
@@ -41,4 +65,3 @@ PartnerOverlapConnections.propTypes = {
 };
 
 export default PartnerOverlapConnections;
-
