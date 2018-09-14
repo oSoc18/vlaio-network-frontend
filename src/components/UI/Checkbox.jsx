98c1d833203c1cd2a8c159764a62c9be1bc26a3e
Alex <alexandervandoren@yahoo.com>
2018-09-14 12:08:41 +0200

    Move Checkbox components

--- src/components/UI/Checkbox.jsx
+++ src/components/UI/Checkbox.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import './style.css';
+import './Checkbox.css';
 
 class Checkbox extends Component {
   constructor(props) {
