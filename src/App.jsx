808cb804d68672d99c9d2f84d949da2e927ebd84
Alex <alexandervandoren@yahoo.com>
2018-09-19 18:07:00 +0200

    Playing with routing for the "demo" tomorrow

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainLayout from './components/MainLayout';
 import Overview from './components/Overview';
 import NotFound from './components/404';
-
+import Ivan from './components/IvansSunburst';
 import 'normalize.css';
 import './assets/styles/index.css';
 
@@ -11,6 +11,7 @@ const App = () => (
   <BrowserRouter>
     <Switch>
       <MainLayout exact path="/:path(|index|home)" component={Overview} />
+      <MainLayout exact path="/:path(|ivan)" component={Ivan} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
---
+++ src/components/IvansSunburst/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Ivan extends Component {
+  state = {
+    overlaps: []
+  };
+
+  render() {
+    return (
+      <div className="main-content overview ivan">
+        Hi
+      </div>
+    );
+  }
+}
+
+export default Ivan;
