712a7cf8a414a0a608a147c09f78546e661efae1
Michiel Leyman <michiel.leyman@gmail.com>
2018-10-02 18:59:03 +0200

    Add login page skeleton

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import Header from '../Header';
+import Footer from '../Footer';
+
+import '../../assets/styles/login.css';
 
 const Login = () => (
-  <p>Log in</p>
+  <div className="main-layout">
+    <Header />
+    <main className="page login">
+      <div>Login</div>
+    </main>
+    <Footer />
+  </div>
 );
 
 export default Login;
