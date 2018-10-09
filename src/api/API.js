a83f55ef5deb8dba730eb01a60e2fe5cc31e6866
Michiel Leyman <michiel.leyman@gmail.com>
2018-10-09 16:02:39 +0200

    Add login method

--- src/api/API.js
+++ src/api/API.js
@@ -6,6 +6,16 @@ class API {
       Accept: 'application/json'
     };
   }
+
+  getOptions(method, authRequired = false, body = null) {
+    const options = {
+      method: method.toUpperCase(),
+      headers: this.headers
+    };
+    if (body) options.body = JSON.stringify(body);
+    // if (authRequired) options.credentials = 'include';
+    return options;
+  }
 }
 
 export default API;
--- src/api/Auth.js
+++ src/api/Auth.js
@@ -3,7 +3,13 @@ import API from './API';
 class AuthAPI extends API {
   constructor(url) {
     super(url);
-    this.endpoint = `${this.BASE_URL}/user/`;
+    this.endpoint = `${this.BASE_URL}/user`;
+    this.tmpurl = 'http://localhost:8000/user/login/';
+  }
+
+  login(username, password) {
+    return fetch(this.tmpurl, this.getOptions('POST', false, { username, password }))
+      .then(r => r.json());
   }
 }
 
