2fcc2fec86560991f983a6946e4c3cfc949308ef
Michiel Leyman <michiel.leyman@gmail.com>
2018-10-10 14:47:49 +0200

    Add User management component

--- src/components/Admin/Manage.jsx
+++ src/components/Admin/Manage.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { api } from '../../constants';
+import Header from '../Header';
+import Footer from '../Footer';
 
-const Manage = () => (
-  <p>Admin interface</p>
-);
+import '../../assets/styles/user-management.css';
+
+class Manage extends Component {
+  state = { users: [] };
+
+  componentDidMount() {
+    api.user.get().then((users) => {
+      this.setState({ users: users.length ? users : [] });
+    });
+  }
+
+  render() {
+    return (
+      <div className="main-layout">
+        <Header />
+        <div className="page-alternative">
+          <main className="user-management">
+            <p>Beheer gebruikers</p>
+          </main>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
 
 export default Manage;
