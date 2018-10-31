fa4cd9d26afc4e7d30bd8760d7df257b79e44884
Michiel Leyman <michiel.leyman@gmail.com>
2018-10-31 17:00:41 +0100

    Allow className prop in IconButton

--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const IconButton = ({ icon, tabIndex, onClick }) => {
+const IconButton = ({
+  icon, tabIndex, onClick, className
+}) => {
   const onKeyDown = (e) => {
     if (e.keyCode === 13) onClick();
   };
+
+  const classes = (className) ? `icon-button ${className}` : 'icon-button';
 
   return (
     <i
@@ -13,7 +17,7 @@ const IconButton = ({ icon, tabIndex, onClick }) => {
       tabIndex={tabIndex}
       onKeyDown={e => onKeyDown(e)}
       onClick={onClick}
-      className="icon-button"
+      className={classes}
     >
       <FontAwesomeIcon icon={icon} />
     </i>
@@ -24,7 +28,8 @@ IconButton.propTypes = {
   // eslint-disable-next-line react/forbid-prop-types
   icon: PropTypes.object.isRequired,
   tabIndex: PropTypes.number.isRequired,
-  onClick: PropTypes.func.isRequired
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string.isRequired
 };
 
 export default IconButton;
