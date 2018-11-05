6327838f9bc585a10b51685ccce4ff9dd1aae645
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-05 13:51:48 +0100

    Create import building blocks

--- src/components/Import/index.jsx
+++ src/components/Import/index.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+import '../../assets/styles/import.css';
+
 const Import = () => (
   <main className="import">
-    Import page
+    <div>
+      <div className="import__progress">
+        <ol>
+          <li>upload</li>
+          <li>controle</li>
+          <li>import</li>
+        </ol>
+      </div>
+      <div className="import__details">
+        blah
+      </div>
+    </div>
   </main>
 );
 
