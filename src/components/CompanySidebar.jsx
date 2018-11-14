893c4c30ee32d6efec40ee6d172e3f4936285fb8
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-14 13:46:50 +0100

    Make CompanySidebar a class component

--- src/components/CompanySidebar.jsx
+++ src/components/CompanySidebar.jsx
@@ -1,50 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Company from '../models/Company';
 
 import '../assets/styles/sidebar.css';
 
-const CompanySidebar = ({ companies }) => {
-  const alphabeticalCompanies = companies.reduce((collection, company) => {
+class CompanySidebar extends Component {
+  groupAlphabetically = companies => companies.reduce((collection, company) => {
     const letter = company.name.trim().charAt(0);
     if (!collection[letter]) collection[letter] = [];
     collection[letter].push(company);
     return collection;
   }, {});
 
-  const renderCompany = company => (
-    <li>{company.name}</li>
-  );
+  renderCompany = company => <li>{company.name}</li>;
 
-  const renderLetterGroup = letter => (
+  renderGroup = (group, companies) => (
     <div className="company-list__companies__collection">
-      <header>{letter}</header>
+      <header>{group}</header>
       <ul>
-        {alphabeticalCompanies[letter].map(renderCompany)}
+        {companies[group].map(this.renderCompany)}
       </ul>
     </div>
   );
 
-  return (
-    <div className="side-nav side-nav--companies">
-      <fieldset>
-        <legend className="main-legend">Zoeken naar bedrijven</legend>
-        <input type="search" className="input" />
-      </fieldset>
+  render() {
+    const alphabeticalCompanies = this.groupAlphabetically(this.props.companies);
+    return (
+      <div className="side-nav side-nav--companies">
+        <fieldset>
+          <legend className="main-legend">Zoeken naar bedrijven</legend>
+          <input type="search" className="input" />
+        </fieldset>
 
-      <fieldset className="company-list">
-        <legend className="main-legend">Bedrijven</legend>
-        <div className="company-list__companies">
-          {
-            Object.keys(alphabeticalCompanies)
-              .sort()
-              .map(renderLetterGroup)
-          }
-        </div>
-      </fieldset>
-    </div>
-  );
-};
+        <fieldset className="company-list">
+          <legend className="main-legend">Bedrijven</legend>
+          <div className="company-list__companies">
+            {
+              Object.keys(alphabeticalCompanies)
+                .sort()
+                .map(letter => this.renderGroup(letter, alphabeticalCompanies))
+            }
+          </div>
+        </fieldset>
+      </div>
+    );
+  }
+}
 
 CompanySidebar.defaultProps = {
   companies: []
