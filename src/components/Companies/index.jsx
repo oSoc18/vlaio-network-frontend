94830ba2fd224c1d78d36c6843f7a56ea9e3aade
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-14 16:17:11 +0100

    Style resulting table

--- src/components/Companies/index.jsx
+++ src/components/Companies/index.jsx
@@ -5,16 +5,34 @@ import Company from '../../models/Company';
 import '../../assets/styles/companies.css';
 
 const Companies = ({ companies, activeCompany }) => (
-  <div>
+  <div className="company-details">
     {activeCompany && (
-      <ul>
-        <li>{activeCompany.VAT}</li>
-        <li>{activeCompany.name}</li>
-        <li>{activeCompany.employeeAmount}</li>
-        <li>{activeCompany.profit}</li>
-      </ul>
-    )
-    }
+      <table>
+        <thead>
+          <tr>
+            <th colSpan="2">{activeCompany.name}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>BTW-nummer</td>
+            <td>{activeCompany.VAT}</td>
+          </tr>
+          <tr>
+            <td>Naam</td>
+            <td>{activeCompany.name}</td>
+          </tr>
+          <tr>
+            <td>Aantal werknemers</td>
+            <td>{activeCompany.employeeAmount}</td>
+          </tr>
+          <tr>
+            <td>Winst</td>
+            <td>{activeCompany.profit}</td>
+          </tr>
+        </tbody>
+      </table>
+    )}
   </div>
 );
 
