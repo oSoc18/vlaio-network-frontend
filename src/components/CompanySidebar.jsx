fd399eef59f3c467bf9ef4cfb1ed4da45e666a2b
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-14 13:35:12 +0100

    Group and render companies alphabetically

--- src/components/CompanySidebar.jsx
+++ src/components/CompanySidebar.jsx
@@ -5,10 +5,43 @@ import Company from '../models/Company';
 import '../assets/styles/sidebar.css';
 
 const CompanySidebar = ({ companies }) => {
-  console.log(companies);
+  const alphabeticalCompanies = companies.reduce((collection, company) => {
+    const letter = company.name.trim().charAt(0);
+    if (!collection[letter]) collection[letter] = [];
+    collection[letter].push(company);
+    return collection;
+  }, {});
+
+  const renderCompany = company => (
+    <li>{company.name}</li>
+  );
+
+  const renderLetterGroup = letter => (
+    <div className="company-list__companies__collection">
+      <header>{letter}</header>
+      <ul>
+        {alphabeticalCompanies[letter].map(renderCompany)}
+      </ul>
+    </div>
+  );
+
   return (
-    <div className="side-nav">
-      Stuff
+    <div className="side-nav side-nav--companies">
+      <fieldset>
+        <legend className="main-legend">Zoeken naar bedrijven</legend>
+        <input type="search" className="input" />
+      </fieldset>
+
+      <fieldset className="company-list">
+        <legend className="main-legend">Bedrijven</legend>
+        <div className="company-list__companies">
+          {
+            Object.keys(alphabeticalCompanies)
+              .sort()
+              .map(renderLetterGroup)
+          }
+        </div>
+      </fieldset>
     </div>
   );
 };
