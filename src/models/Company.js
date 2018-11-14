42446f8542d8f4cadadffe6755e4005f5ce01c6a
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-14 11:25:57 +0100

    Add Company model

---
+++ src/models/Company.js
@@ -0,0 +1,11 @@
+class Company {
+  constructor(company) {
+    this.id = company.vat;
+    this.VAT = company.vat;
+    this.name = company.name;
+    this.employeeAmount = company.employeeAmount;
+    this.profit = company.profit;
+  }
+}
+
+export default Company;
