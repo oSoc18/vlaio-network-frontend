3611f2066c2c1fdc567edf803c837a43ef8eb965
Michiel Leyman <michiel.leyman@gmail.com>
2018-11-15 20:41:44 +0100

    Add empty state to company page

--- src/components/Companies/index.jsx
+++ src/components/Companies/index.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Company from '../../models/Company';
+import EmptyState from '../UI/states/Empty';
 
 import '../../assets/styles/companies.css';
 
-const Companies = ({ companies, activeCompany }) => (
-  <div className="company-details">
-    {activeCompany && (
-      <table>
-        <thead>
-          <tr>
-            <th colSpan="2">{activeCompany.name}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>BTW-nummer</td>
-            <td>{activeCompany.VAT}</td>
-          </tr>
-          <tr>
-            <td>Naam</td>
-            <td>{activeCompany.name}</td>
-          </tr>
-          <tr>
-            <td>Aantal werknemers</td>
-            <td>{activeCompany.employeeAmount}</td>
-          </tr>
-          <tr>
-            <td>Winst</td>
-            <td>{activeCompany.profit}</td>
-          </tr>
-        </tbody>
-      </table>
-    )}
-  </div>
-);
+const Companies = ({ companies, activeCompany }) => {
+  if (!activeCompany) {
+    return (
+      <EmptyState
+        message="Kies een bedrijf uit de zijbalk om de informatie te tonen"
+      />
+    );
+  }
+
+  return (
+    <div className="company-details">
+      {activeCompany && (
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="2">{activeCompany.name}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>BTW-nummer</td>
+              <td>{activeCompany.VAT}</td>
+            </tr>
+            <tr>
+              <td>Naam</td>
+              <td>{activeCompany.name}</td>
+            </tr>
+            <tr>
+              <td>Aantal werknemers</td>
+              <td>{activeCompany.employeeAmount}</td>
+            </tr>
+            <tr>
+              <td>Winst</td>
+              <td>{activeCompany.profit}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
 
 Companies.defaultProps = {
   companies: [],
