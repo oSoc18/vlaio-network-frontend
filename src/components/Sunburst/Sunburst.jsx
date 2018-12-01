28dd36994c9889e9ac8f197d71b85e0a9cb4be84
IvanSladkov <i_sladkov@hotmail.com>
2018-12-01 20:27:36 +0100

    Fixed interaction and partner buttons and added max depth

--- src/components/Sunburst/Sunburst.jsx
+++ src/components/Sunburst/Sunburst.jsx
@@ -12,36 +12,38 @@ class SunburstChart extends Component {
     ugent: '#b2df8a',
     kuleuven: '#33a02c',
     voka: '#e31a1c',
-    unizo: '#fdbf6f'
+    unizo: '#fdbf6f',
+    vub: '#882255'
   };
 
   constructor(props) {
     super(props);
-    this.refreshSunburst(props);
+    this.refreshStyle(false, props.data);
+    this.maxDepth = 3;
+    this.state = {
+      data: props.data,
+      zoomedData: this.limitDataDepth(props.data),
+      zoomed: false,
+      hoveredCell: false,
+      fullPath: [props.data.name],
+      path: [],
+      hoveredValue: null
+    };
   }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.data !== this.state.data) this.refreshSunburst(prevProps);
+  componentWillUpdate(nextProps) {
+    if (nextProps.data.name !== this.state.data.name) this.refreshSunburst(nextProps);
   }
 
   refreshSunburst = (props) => {
-    this.state = {
-      data: props.data,
-      zoomedData: props.data,
-      zoomed: false,
-      selected: false,
-      hoveredCell: false,
-      fullPath: [],
-      path: [],
-      hoveredValue: null,
-      colours: {}
-    };
-    if (localStorage.getItem('colorMap') === null) {
-      localStorage.setItem('colorMap', JSON.stringify(this.colorMap));
-    }
-    this.state.colours = JSON.parse(localStorage.getItem('colorMap'));
-    this.state.fullPath.push(this.state.data.name);
     this.refreshStyle(false, props.data);
+    this.setState({
+      data: props.data,
+      zoomedData: this.limitDataDepth(props.data),
+      zoomed: false,
+      fullPath: [props.data.name],
+      path: []
+    });
   };
 
   // gets the path to the selected node in the json
@@ -63,7 +65,10 @@ class SunburstChart extends Component {
     }
     // inserts node information the first time the data is loaded
     if (node.color === undefined) {
-      const { colours } = this.state;
+      if (localStorage.getItem('colorMap') === null) {
+        localStorage.setItem('colorMap', JSON.stringify(this.colorMap));
+      }
+      const colours = JSON.parse(localStorage.getItem('colorMap'));
       if (colours[node.name.toLowerCase()] === undefined) {
         colours[node.name.toLowerCase()] = chroma.scale(['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#e31a1c', '#fdbf6f'])(Math.random()).hex(); // '#'+((1<<24)*Math.random()|0).toString(16);
         localStorage.setItem('colorMap', JSON.stringify(colours));
@@ -105,18 +110,38 @@ class SunburstChart extends Component {
     return data;
   };
 
-  zoomIn = (path, zoomedData) => {
-    let zoomed = zoomedData;
+  zoomIn = (path) => {
+    const { data } = this.state;
+    let zoomed = data;
     path.forEach((crumb) => {
       const index = zoomed.children.findIndex(child => child.name === crumb);
       zoomed = zoomed.children[index];
     });
-    return zoomed;
+    return this.limitDataDepth(zoomed);
+  };
+
+  limitDataDepth = (data, maxDepth = this.maxDepth) => {
+    let dataToLimit = JSON.parse(JSON.stringify(data));
+    if (maxDepth === 0) {
+      dataToLimit.children = [];
+      console.log(dataToLimit);
+      return dataToLimit;
+    }
+    const newMaxDepth = maxDepth - 1;
+
+    //dataToLimit.children.forEach(child => this.limitDataDepth(child, newMaxDepth));
+    for (let i = 0; i < dataToLimit.children.length; i++) {
+      dataToLimit.children[i] = this.limitDataDepth(dataToLimit.children[i], newMaxDepth);
+    }
+
+    console.log('last');
+    console.log(dataToLimit);
+    return dataToLimit;
   };
 
   render() {
     const {
-      selected, zoomed, fullPath, path, data, zoomedData, hoveredCell, hoveredValue
+      zoomed, fullPath, path, zoomedData, hoveredCell, hoveredValue
     } = this.state;
     const { height, width } = this.props;
     return (
@@ -132,13 +157,13 @@ class SunburstChart extends Component {
               this.setState({
                 fullPath: fullPath.concat(path),
                 zoomed: true,
-                zoomedData: this.zoomIn(path, zoomedData)
+                zoomedData: this.zoomIn(fullPath.slice(1, fullPath.length).concat(path))
               });
             } else {
               this.setState({
                 fullPath: fullPath.slice(0, -1),
                 zoomed: fullPath.slice(0, -1).length !== 1,
-                zoomedData: this.zoomIn(fullPath.slice(1, fullPath.length - 1), data)
+                zoomedData: this.zoomIn(fullPath.slice(1, fullPath.length - 1))
               });
             }
           }}
