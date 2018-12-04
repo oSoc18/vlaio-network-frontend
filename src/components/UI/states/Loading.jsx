11b06c618b9a518a8fe740d74959b0b81ff958ee
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-04 12:43:58 +0100

    Add loading state to overlap

--- src/components/UI/states/Loading.jsx
+++ src/components/UI/states/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import '../../../assets/styles/UI/states.css';
 
 const LoadingState = () => (
-  <div className="state">
+  <div className="state--loading">
     <div className="ellipsis-loader">
       <div /><div /><div /><div />
     </div>
