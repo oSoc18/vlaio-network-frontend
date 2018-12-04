0a5f75b245694dd5dfe4e2397db114b395e38523
Michiel Leyman <michiel.leyman@gmail.com>
2018-12-04 12:30:40 +0100

    Add loading state

---
+++ src/components/UI/states/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../../../assets/styles/UI/states.css';
+
+const LoadingState = () => (
+  <div className="state">
+    <div className="ellipsis-loader">
+      <div /><div /><div /><div />
+    </div>
+  </div>
+);
+
+export default LoadingState;
